8d663c98662f3712a3a1d4c5842dfcaebb84d741
Vince-maple-byte <73848683+Vince-maple-byte@users.noreply.github.com>
2025-06-07 14:00:24 -0400

    Was able to fix the billing service properties file to be able to do postgres sql queries.

--- patient/src/main/java/com/pms/patient/config/GrpcClientConfig.java
+++ patient/src/main/java/com/pms/patient/config/GrpcClientConfig.java
@@ -1,10 +1,11 @@
 package com.pms.patient.config;
 
 import billing.BillingServiceGrpc;
-import io.grpc.ManagedChannel;
+import io.grpc.*;
 import io.grpc.stub.AbstractStub;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.grpc.client.BlockingStubFactory;
 import org.springframework.grpc.client.GrpcChannelFactory;
 import org.springframework.grpc.client.StubFactory;
 
@@ -13,10 +14,17 @@ import java.util.function.Supplier;
 @Configuration
 public class GrpcClientConfig {
 
+
+
     //I'll make some notes on stub factories here
     @Bean
     billing.BillingServiceGrpc.BillingServiceBlockingStub stub(GrpcChannelFactory channels) {
-        //BillingServiceGrpc.BillingServiceBlockingStub.newStub("local", channels.createChannel("local"));
+//        StubFactory<BillingServiceGrpc.BillingServiceBlockingStub> factory =
+//                BillingServiceGrpc::newBlockingStub;
+
+//        BillingServiceGrpc.BillingServiceBlockingStub stub =
+//                channels.createStub("local", BillingServiceGrpc::newBlockingStub);
+
 
         return billing.BillingServiceGrpc.newBlockingStub(channels.createChannel("local"));
         //return BillingServiceGrpc.newStub(channels.createChannel("local"));
